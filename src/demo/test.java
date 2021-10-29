02bd8c66579933928d678a73b893fd7e431ce410
Manishsarkate <91876880+Manishsarkate@users.noreply.github.com>
2021-10-29 11:10:17 +0530

    function program commit

---
+++ src/demo/test.java
@@ -0,0 +1,35 @@
+package demo;
+
+import java.util.Scanner;
+class demo
+{
+	public static void main(String[] args) 
+	{
+		
+		System.out.println("Additio university");		
+
+	}
+
+	
+}
+
+public class test 
+{
+
+	public static void main(String[] args) 
+	{
+		int a,b,c;
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter the value of a:");
+		a=s.nextInt();
+		System.out.println("Enter the value of b:");
+		b=s.nextInt();
+		c=a+b;
+		System.out.println("Addition of two number:"+c);
+		
+		
+		
+
+	}
+
+}
